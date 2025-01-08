7ce7a641314cc9110cf8905f8497862c0d563458
Devon-Henry <98130984+Devon-Henry@users.noreply.github.com>
2025-01-08 10:39:07 +0000

    Update removeTag.ps1

--- automations/removeTag.ps1
+++ automations/removeTag.ps1
@@ -1,8 +1,11 @@
 # Connect to Azure account using the System/User assigned Identity (must provide appropriate permissions on automation account in specified resource group)
 Connect-AzAccount -Identity
 
+# Grabs Variable stored in azure automation account
+$resourcegroup = Get-AutomationVariable -Name "avdresourcegrp"
+
 # Get the resources that fit the criteria
-$resources = Get-AzResource -ResourceGroupName "your-resource-group-here" -ResourceType Microsoft.Compute/virtualMachines 
+$resources = Get-AzResource -ResourceGroupName $resourcegroup -ResourceType Microsoft.Compute/virtualMachines 
 
 # Loop through each resource in $resources variable
 foreach ($resrc in $resources) {
